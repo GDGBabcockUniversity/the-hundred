1a63fc21a80574317f5ce67b15b238ece2b602d9
Victor Ibironke <victoribironke2019@gmail.com>
2026-05-06 09:20:35 +0100

    chore: refactor ESLint configuration to streamline imports and enhance code readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,18 @@
-// import { defineConfig, globalIgnores } from "eslint/config";
-// import nextVitals from "eslint-config-next/core-web-vitals";
-// import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-// const eslintConfig = defineConfig([
-//   ...nextVitals,
-//   ...nextTs,
-//   // Override default ignores of eslint-config-next.
-//   globalIgnores([
-//     // Default ignores of eslint-config-next:
-//     ".next/**",
-//     "out/**",
-//     "build/**",
-//     "next-env.d.ts",
-//   ]),
-// ]);
-
-// export default eslintConfig;
-
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
-const eslintConfig = [
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
@@ -43,6 +20,6 @@ const eslintConfig = [
       "@next/next/no-img-element": "off",
     },
   },
-];
+]);
 
 export default eslintConfig;
